47c04af53b6af3c4e60458e2b5938b606e1f89bf
Julie <julie.albini@edu.univ-fcomte.fr>
2026-07-01 09:36:05 +0200

    [TASK] [BACKEND] Suppression de validator.js car redondant

--- server/utils/validators.js
+++
@@ -1,9 +0,0 @@
-// Regex standard pour valider un UUID v4 (format utilisé par Supabase)
-const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
-
-/**
- * Vérifie qu'une chaîne est un UUID valide
- */
-export function isValidUUID(value) {
-  return typeof value === 'string' && UUID_REGEX.test(value)
-}
